9aa4636d248218eefd89720afb5ad40c0dabb80f
sekito <sekito1107gg@gmail.com>
2026-02-05 01:54:34 +0900

    Config: CodeMirrorを削除しAce Editorに変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,23 +6,8 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# CodeMirror 6 & Dependencies (via esm.sh stable)
-pin "codemirror", to: "https://esm.sh/codemirror@6.0.1"
-pin "@codemirror/view", to: "https://esm.sh/@codemirror/view@6.9.0"
-pin "@codemirror/state", to: "https://esm.sh/@codemirror/state@6.2.0"
-pin "@codemirror/language", to: "https://esm.sh/@codemirror/language@6.6.0"
-pin "@codemirror/commands", to: "https://esm.sh/@codemirror/commands@6.2.2"
-pin "@codemirror/search", to: "https://esm.sh/@codemirror/search@6.2.3"
-pin "@codemirror/autocomplete", to: "https://esm.sh/@codemirror/autocomplete@6.4.2"
-pin "@codemirror/lint", to: "https://esm.sh/@codemirror/lint@6.1.1"
-pin "@codemirror/lang-ruby", to: "https://esm.sh/@codemirror/lang-ruby@6.0.1"
-pin "@codemirror/theme-one-dark", to: "https://esm.sh/@codemirror/theme-one-dark@6.1.1"
-
-# Dependencies
-pin "@lezer/common", to: "https://esm.sh/@lezer/common@1.0.2"
-pin "@lezer/highlight", to: "https://esm.sh/@lezer/highlight@1.1.3"
-pin "@lezer/lr", to: "https://esm.sh/@lezer/lr@1.3.3"
-pin "@lezer/ruby", to: "https://esm.sh/@lezer/ruby@0.2.0"
-pin "style-mod", to: "https://esm.sh/style-mod@4.0.0"
-pin "w3c-keyname", to: "https://esm.sh/w3c-keyname@2.2.6"
-pin "crelt", to: "https://esm.sh/crelt@1.0.5"
+# Ace Editor (via jsdelivr)
+pin "ace-builds", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/ace.js"
+pin "ace-builds/mode/ruby", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/mode-ruby.js"
+pin "ace-builds/theme/chrome", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/theme-chrome.js"
+pin "ace-builds/theme/one_dark", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/theme-one_dark.js"
