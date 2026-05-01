8efa0b71c742b7d5d0694b0a556b2c340fe90ac2
sekito <sekito1107gg@gmail.com>
2026-05-02 00:31:00 +0900

    src/App.tsxの作成

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1 className="text-2xl font-bold text-red-600">Rubox Rebuild</h1>
+    </div>
+  )
+}
+
+export default App
