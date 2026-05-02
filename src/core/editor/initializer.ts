6d967fa2a6873f6c8a6658fce11616d7fa040e0d
sekito <sekito1107gg@gmail.com>
2026-05-02 09:22:55 +0900

    editor initializer の作成

---
+++ src/core/editor/initializer.ts
@@ -0,0 +1,24 @@
+import * as monaco from 'monaco-editor';
+import { editor, updateCode } from '../../state/editor';
+import { app } from '../../state/app';
+
+export function run(htmlElement: HTMLElement) {
+  const instance = monaco.editor.create(htmlElement, {
+    value: editor.code,
+    language: 'ruby',
+    theme: app.theme === 'dark' ? 'vs-dark' : 'vs',
+    automaticLayout: true,
+    fontSize: 14,
+    fontFamily: 'JetBrains Mono, Menlo, Monaco, "Courier New", monospace',
+    minimap: { enabled: false },
+    scrollBeyondLastLine: false,
+    lineNumbers: 'on',
+    padding: { top: 16, bottom: 16 },
+  });
+
+  instance.onDidChangeModelContent(() => {
+    updateCode(instance.getValue());
+  });
+
+  return () => instance.dispose();
+}
