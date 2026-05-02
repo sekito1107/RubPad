a0af1737851aa456926ecaa38d118efc43afcb54
sekito <sekito1107gg@gmail.com>
2026-05-02 13:37:05 +0900

    YARVサーバーのTop-level_await化

--- src/core/yarv/server.ts
+++ src/core/yarv/server.ts
@@ -4,40 +4,28 @@ import initRubyWasm from '../../../public/ruby/rubox.wasm?init';
 
 type RubyVM = Awaited<ReturnType<typeof DefaultRubyVM>>['vm'];
 
-(async () => {
-  try {
-    const module = await initRubyWasm();
-    const { vm } = await DefaultRubyVM(module);
-    vm.eval('require "js"');
+const module = await initRubyWasm();
+const { vm } = await DefaultRubyVM(module);
+vm.eval('require "js"');
 
-    postMessage({ type: 'ready' });
+postMessage({ type: 'ready' });
 
-    self.onmessage = (event) => handleRequest(vm, event);
-  } catch (e) {
-    const payload = e instanceof Error ? e.message : String(e);
-    postMessage({ type: 'error', payload: `Failed to initialize Ruby VM: ${payload}` });
-  }
-})();
+self.onmessage = event => handleRequest(vm, event);
 
 function handleRequest(vm: RubyVM, event: MessageEvent) {
   const { code } = event.data;
+  const escapedCode = JSON.stringify(code);
 
-  try {
-    (self as any)._tmpCode = code;
+  const result = vm.eval(`
+    require "stringio"
+    $stdout = StringIO.new
+    begin
+      eval(${escapedCode})
+    rescue Exception => e
+      puts e.full_message
+    end
+    $stdout.string
+  `);
 
-    // StringIOで$stdoutを上書きして実行結果を回収する
-    const result = vm.eval(`
-      require "stringio"
-      $stdout = StringIO.new
-      eval(JS.global[:_tmpCode].to_s)
-      $stdout.string
-    `);
-
-    postMessage({ type: 'output', payload: result.toString() });
-  } catch (e) {
-    const payload = e instanceof Error ? e.message : String(e);
-    postMessage({ type: 'error', payload });
-  } finally {
-    (self as any)._tmpCode = null;
-  }
+  postMessage({ type: 'output', payload: result.toString() });
 }
