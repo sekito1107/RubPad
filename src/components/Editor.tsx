6eb6816ec5d4cb2c17c9720e870bf1abe486a0ba
sekito <sekito1107gg@gmail.com>
2026-05-03 01:59:31 +0900

    エディタの初期化方式をuseEditorフックへ移行

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,12 @@
-import { initialize } from '../core/editor';
+import { useEditor } from '../hooks/useEditor';
 
 export default function Editor() {
+  useEditor();
+
   return (
-    <div 
-      id="monaco-editor" 
-      className="w-full h-full" 
-      ref={initialize} 
+    <div
+      id="monaco-editor"
+      className="w-full h-full"
     />
   );
 }
