58213bf002099051977db072b761386f284994e0
sekito <sekito1107gg@gmail.com>
2026-05-04 06:53:47 +0900

    コード実行テストにエラー検証を追加

--- tests/system/code_execution_test.rb
+++ tests/system/code_execution_test.rb
@@ -16,4 +16,20 @@ class CodeExecutionTest < SystemTest
       assert_text "Hello Rubox"
     end
   end
+
+  def test_不正なRubyコードを実行した際にエラーが出力されること
+    visit "/"
+
+    assert_selector "#run-button:not([disabled])"
+
+    find(".monaco-editor").click
+    send_keys([:control, "a"], :backspace)
+    send_keys("1 +")
+
+    click_on "run-button"
+
+    within "#terminal-panel" do
+      assert_text "syntax error"
+    end
+  end
 end
