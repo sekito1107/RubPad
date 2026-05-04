c4c8a0fc8610ccd42381df4a81919864b0a3f656
sekito <sekito1107gg@gmail.com>
2026-05-04 15:27:26 +0900

    TypeProfのロード環境整備とboot.rbの整理

--- src/core/ruby/yarv/boot.rb
+++ src/core/ruby/yarv/boot.rb
@@ -1,12 +1,7 @@
+$LOADED_FEATURES << "socket" << "io/console"
+
 require "js"
 require "stringio"
 require "rubygems"
-
-
-begin
-  require "typeprof"
-  require "typeprof/lsp"
-  puts "Rubox: TypeProf & LSP loaded successfully."
-rescue LoadError => e
-  warn "Rubox Warning: TypeProf or LSP could not be loaded. Analysis features will be disabled. (#{e.message})"
-end
+require "typeprof"
+require "typeprof/lsp"
