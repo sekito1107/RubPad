3f4e264377d0d22e1990f06a293c7b789aaf8aea
sekito <sekito1107gg@gmail.com>
2026-05-05 16:30:58 +0900

    TypeProfの初期化のみを担うTypeProfEngineモジュールを作成

---
+++ src/core/ruby/typeprof.rb
@@ -0,0 +1,32 @@
+$LOADED_FEATURES << "socket" << "io/console"
+
+require "js"
+require "pathname"
+require "rubygems"
+require "rbs"
+require "typeprof"
+
+module TypeProfEngine
+  class << self
+    def service
+      @service
+    end
+
+    def init
+      return if @service || @initializing
+      @initializing = true
+
+      JS.global.fetch("/rbs/ruby-stdlib.rbs")
+        .then { |res| res.text }
+        .then do |rbs_data|
+          File.write("/stdlib.rbs", rbs_data.to_s)
+
+          loader = RBS::EnvironmentLoader.new(core_root: nil)
+          loader.add(path: Pathname.new("/stdlib.rbs"))
+          @service = TypeProf::Core::Service.new(rbs_env: RBS::Environment.from_loader(loader))
+
+          @initializing = false
+        end
+    end
+  end
+end
