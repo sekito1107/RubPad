2ff3e5468443e2391c4cc361520eec7d9e444804
sekito <sekito1107gg@gmail.com>
2026-05-05 15:48:17 +0900

    EditorコンポーネントでuseAnalysisフックを呼び出すように修正

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,9 @@
 import { useDiagnostics } from '../hooks/useDiagnostics';
+import { useAnalysis } from '../hooks/useAnalysis';
 
 export default function Editor() {
   useDiagnostics();
+  useAnalysis();
 
   return (
     <div
