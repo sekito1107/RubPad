01fffe3cd195b97fa09c24a826a2a15b26105c0b
sekito <sekito1107gg@gmail.com>
2026-05-06 10:35:06 +0900

    Ruby解析エンジンのリファクタリング：RBS属性（attrs）を活用した堅牢なメタデータ抽出の実装

--- src/core/ruby/typeprof/analyzer.rb
+++ src/core/ruby/typeprof/analyzer.rb
@@ -34,18 +34,10 @@ module Analyzer
     private
 
     def add_method(name, node)
-      @methods << create_entry(name, node)
-    end
-
-    def add_variable(name, node)
-      @variables << create_entry(name, node)
-    end
-
-    def create_entry(name, node)
       query_loc = (node.message_loc if node.respond_to?(:message_loc)) || node.location
       info = resolve_info(query_loc)
 
-      {
+      @methods << {
         name: name.to_s,
         line: query_loc.start_line,
         col: query_loc.start_column,
@@ -53,10 +45,19 @@ module Analyzer
       }
     end
 
+    def add_variable(name, node)
+      loc = node.location
+      @variables << {
+        name: name.to_s,
+        line: loc.start_line,
+        col: loc.start_column
+      }
+    end
+
     def resolve_info(loc)
       return nil unless (service = @service)
-
       pos = TypeProf::CodePosition.new(loc.start_line, loc.start_column)
+
       # TypeProf 内部の解析済み AST キャッシュに直接アクセス (非公開 API)
       root = service.instance_variable_get(:@rb_text_nodes)["main.rb"]
       
@@ -69,9 +70,8 @@ module Analyzer
       end
       
       info ||= {
-        owner: service.hover("main.rb", pos),
-        method_name: nil,
-        owner_type: "class",
+        owner: nil,
+        owner_type: nil,
         is_singleton_call: false,
         has_instance: false,
         has_singleton: false
@@ -80,17 +80,17 @@ module Analyzer
     end
 
     def method_call_node?(node)
-      node.is_a?(Prism::CallNode) || node.is_a?(Prism::SymbolNode)
+      !!node&.boxes(:mcall) { break true }
     end
 
     def resolve_method_call(node, service)
       info = nil
       node.boxes(:mcall) do |box|
         box.resolve(service.genv, nil) do |me, _ty, mid, orig_ty|
-          info = extract_method_metadata(me, service)
+          next unless me
 
+          info = extract_method_metadata(me, service)
           info[:owner] ||= orig_ty&.base_type(service.genv)&.show
-          info[:method_name] = mid.to_s
         end
       end
       info
@@ -99,35 +99,20 @@ module Analyzer
     private
 
     def extract_method_metadata(me, service)
-      return {} unless me
-
-      # 公式定義 (decls) または 実定義 (defs) のいずれかから代表的な一つを取得
-      # (TypeProf の内部的な Set クラスは .first を持たないため、一度配列化してから取得)
       source = me.decls.to_a.first || me.defs.to_a.first
-      return {} unless source
+      mod = service.genv.resolve_cpath(source.cpath)
+
+      # RBS の属性を取得 (SigDefNode#attrs)
+      node = source.instance_variable_get(:@node)
+      attrs = node&.respond_to?(:attrs) ? node.attrs : {}
 
       {
-        owner: source.respond_to?(:cpath) ? source.cpath.join("::") : nil,
-        owner_type: get_owner_type(source.mod, service),
-        is_singleton_call: !!source&.singleton?,
-        has_instance: !!source.mod&.get_method(me.mid, false),
-        has_singleton: !!source.mod&.get_method(me.mid, true)
+        owner: source.cpath.join("::"),
+        owner_type: (mod&.module? ? "module" : "class"),
+        is_singleton_call: !!source.singleton,
+        has_instance: !!attrs[:instance],
+        has_singleton: !!attrs[:singleton]
       }
-    end
-
-    def get_owner_type(mod, service)
-      return "class" unless mod.respond_to?(:cpath)
-      
-      last_name = mod.cpath.last
-      namespace = RBS::Namespace.new(path: mod.cpath[0...-1], absolute: true)
-      typename = RBS::TypeName.new(name: last_name, namespace: namespace)
-      
-      decl = service.rbs_env.class_decls[typename]
-      if decl && decl.entry.is_a?(RBS::Environment::ModuleEntry)
-        "module"
-      else
-        "class"
-      end
     end
   end
 
