374a5ef1b90fd5b953f7fb6dba834aae69a68170
sekito <sekito1107gg@gmail.com>
2026-05-10 07:53:26 +0900

    システムテストの安定化とシャドウイングテストの追加

--- tests/system/inlay_hints_test.rb
+++ tests/system/inlay_hints_test.rb
@@ -14,76 +14,83 @@ class InlayHintsTest < SystemTest
 
   def test_変数代入時の遷移表示
     type_code("a = 1")
-    find(".monaco-editor .mtk1", text: "a").hover
+    find(".monaco-editor .view-line", text: "a").find("span", text: "a", exact_text: true).hover
     find("a", text: "値を確認: a").click
     assert_text "a: nil -> 1"
   end
 
   def test_メソッド呼び出し時の遷移表示
     type_code("a = 'hi'\na.upcase")
-    find(".monaco-editor .mtk1", text: "upcase").hover
+    find(".monaco-editor .view-line", text: "upcase").find("span", text: "upcase", exact_text: true).hover
     find("a", text: "値を確認: a.upcase").click
     assert_text "a.upcase: \"hi\" -> \"HI\""
   end
 
   def test_破壊的メソッドでの副作用回避
     type_code("a = []\na << 1")
-    find(".monaco-editor .mtk1", text: "<<").hover
+    find(".monaco-editor .view-line", text: "<<").find("span", text: "<<", match: :first).hover
     find("a", text: "値を確認: a << 1").click
     assert_text "a << 1: [] -> [1]"
   end
 
   def test_破壊的メソッド呼び出し行での変数参照
     type_code("a = [1, 2]\nb = a.pop")
-    all(".monaco-editor .mtk1", text: "a", exact_text: true)[1].hover
+    find(".monaco-editor .view-line", text: "a.pop").find("span", text: "a", match: :first).hover
     find("a", text: "値を確認: a").click
     assert_text "a: [1, 2]"
   end
 
   def test_破壊的メソッド呼び出し行での代入先
     type_code("a = [1, 2]\nb = a.pop")
-    find(".monaco-editor .mtk1", text: "b").hover
+    find(".monaco-editor .view-line", text: "b = a.pop").find("span", text: "b", exact_text: true).hover
     find("a", text: "値を確認: b").click
     assert_text "b: nil -> 2"
   end
 
   def test_長い変数名の省略表示
     type_code("a_very_long_variable_name = 1")
-    find(".monaco-editor .mtk1", text: "a_very_long_variable_name").hover
+    find(".monaco-editor .view-line", text: "a_very_long_variable_name").find("span", text: "a_very_long_variable_name", exact_text: true).hover
     find("a", text: "値を確認: a_very_long_variable_name").click
     assert_text "a_very_long_...: nil -> 1"
   end
 
   def test_メソッドチェーンの中間状態の表示
     type_code("a = 'hi'\na.upcase.reverse")
-    find(".monaco-editor .mtk1", text: "upcase").hover
+    find(".monaco-editor .view-line", text: "a.upcase.reverse").find("span", text: "upcase", exact_text: true).hover
     find("a", text: "値を確認: a.upcase").click
     assert_text "a.upcase: \"hi\" -> \"HI\""
   end
 
   def test_同一行内での複数変数のインスペクト
-    type_code("a = 1; b = 2")
+    # 同一行に複数の式を並べる際、現在のTextMateエンジンの制限により
+    # セミコロンやスペースと変数が一つのspanに結合されてしまい、ホバー座標がズレる場合がある。
+    # インスタンス変数(@b)を使用することでトークンを強制的に分離させ、
+    # かつEscapeキーでホバー状態をリセットすることで、確実に次の要素をインスペクトできるようにしている。
+    type_code("a = 1; @b = 2")
     
-    find(".monaco-editor .mtk1", text: "a").hover
+    find(".monaco-editor .view-line", text: "a = 1; @b = 2").find("span", text: "a", exact_text: true).hover
     find("a", text: "値を確認: a").click
     assert_text "a: nil -> 1"
     
-    find(".monaco-editor .mtk1", text: "b").hover
-    find("a", text: "値を確認: b").click
-    assert_text "b: nil -> 2"
+    # Escapeキーでホバーをリセット
+    send_keys(:escape)
+    
+    find(".monaco-editor .view-line", text: "a = 1; @b = 2").find("span", text: "b", exact_text: true).hover
+    find("a", text: "値を確認: @b").click
+    assert_text "@b: nil -> 2"
     refute_text "a: nil -> 1"
   end
 
   def test_ブロック引数のシャドウイング
-    type_code("a = 0\n[1].each { |a| a + 1 }")
-    all(".monaco-editor .mtk1", text: "a", exact_text: true)[1].hover
+    type_code("a = 0\n[1].each { |a| a }")
+    find(".monaco-editor .view-line", text: "[1].each").all("span", text: "a").last.hover
     find("a", text: "値を確認: a").click
     assert_text "a: 1"
   end
 
   def test_編集によるヒントの消去
     type_code("a = 1")
-    find(".monaco-editor .mtk1", text: "a").hover
+    find(".monaco-editor .view-line", text: "a = 1").find("span", text: "a", exact_text: true).hover
     find("a", text: "値を確認: a").click
     assert_text "a: nil -> 1"
     
@@ -93,7 +100,7 @@ class InlayHintsTest < SystemTest
 
   def test_他行の編集によるヒントの維持
     type_code("a = 1\n")
-    find(".monaco-editor .mtk1", text: "a").hover
+    find(".monaco-editor .view-line", text: "a = 1").find("span", text: "a", exact_text: true).hover
     find("a", text: "値を確認: a").click
     assert_text "a: nil -> 1"
     
