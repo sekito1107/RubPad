36223f2526843f0a8629c287201f1739c2421846
sekito <sekito1107gg@gmail.com>
2026-05-12 06:50:22 +0900

    システムテストのウィンドウサイズを拡大し、ターミナルパネルの視認性を修正

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "capybara/minitest"
 require "rack"
+require "selenium-webdriver"
 
 Capybara.app = Rack::Static.new(
   lambda { [404, {}, []] },
@@ -8,6 +9,15 @@ Capybara.app = Rack::Static.new(
   root: "dist",
   index: "index.html"
 )
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
 
 Capybara.default_driver = :selenium_chrome_headless
 Capybara.default_max_wait_time = 5
